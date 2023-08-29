c04f172a61a780905398bd96811baab9356bdd4f
Stas Volkov <ttv.dev.spirit@gmail.com>
2023-08-29 23:50:02 +0700

    background mover

---
+++ Assets/Scripts/Infrastructure/BackgroundMover.cs
@@ -0,0 +1,56 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using System.Linq;
+using Cinemachine;
+using UnityEngine;
+using Zenject;
+
+namespace HamletTwoSacks.Infrastructure
+{
+    public sealed class BackgroundMover : MonoBehaviour
+    {
+        private CameraController _cameraController = null!;
+
+        private float _minX;
+        private float _maxX;
+        private float _horizontalCameraSize;
+        private CinemachineBrain _brain;
+
+        [SerializeField]
+        private SpriteRenderer _background = null!;
+
+        [SerializeField]
+        private PolygonCollider2D _levelBounds = null!;
+
+        [SerializeField]
+        private CinemachineVirtualCamera _virtualCamera = null!;
+
+        [Inject]
+        private void Construct(CameraController cameraController)
+        {
+            _cameraController = cameraController;
+            _brain = _cameraController.Camera.GetComponent<CinemachineBrain>();
+        }
+
+        private void Awake()
+        {
+            List<float> points = _levelBounds.points.Select(point => point.x).Distinct().ToList();
+            _horizontalCameraSize = _virtualCamera.m_Lens.OrthographicSize * Screen.width / Screen.height;
+
+            _minX = points.Min() + _horizontalCameraSize;
+            _maxX = points.Max() - _horizontalCameraSize;
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 cameraPos = _brain.CurrentCameraState.FinalPosition;
+            float p = (cameraPos.x - _minX) / (_maxX - _minX);
+            var position = _background.transform.position;
+            position.x = (_background.transform.localScale.x - 2 * _horizontalCameraSize) * (2 * p - 1) / 2f
+                         - cameraPos.x;
+            position.x *= -1f;
+            _background.transform.position = position;
+        }
+    }
+}
