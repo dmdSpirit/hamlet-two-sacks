6c99e1ea3d6cece4c732bd180febe2f74d2e377e
dmdspirit <doomedspirit.prog@gmail.com>
2023-09-09 15:44:01 +0700

    actions reader

---
+++ Assets/Scripts/Character/ActionReceiver.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+using System;
+
+namespace HamletTwoSacks.Character
+{
+    public sealed class ActionReceiver
+    {
+        public readonly Action Callback;
+        public readonly string CallToAction;
+        
+        public ActionReceiver(Action callback, string callToAction)
+        {
+            Callback = callback;
+            CallToAction = callToAction;
+        }
+    }
+}
