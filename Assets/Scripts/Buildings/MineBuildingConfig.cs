85703bdda7756e8f2c7aa28fcc03e74b7c634845
dmdspirit <doomedspirit.prog@gmail.com>
2023-09-11 13:02:10 +0700

    config list

---
+++ Assets/Scripts/Buildings/MineBuildingConfig.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HamletTwoSacks.Buildings
+{
+    [CreateAssetMenu(menuName = "Config/Buildings/Mine", fileName = "mine_config", order = 0)]
+    public sealed class MineBuildingConfig : ScriptableObject
+    {
+        [SerializeField]
+        private MineTierInfo[]? _tierInfos;
+
+        public IReadOnlyList<MineTierInfo>? TierInfos => _tierInfos;
+    }
+}
---
+++ Assets/Scripts/Infrastructure/StaticData/ConfigList.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HamletTwoSacks.Infrastructure.StaticData
+{
+    [CreateAssetMenu(menuName = "Config/Config List", fileName = "config_list", order = 0)]
+    public sealed class ConfigList : ScriptableObject
+    {
+        [SerializeField]
+        private ScriptableObject[] _configs = null!;
+
+        public IReadOnlyList<ScriptableObject> Configs => _configs;
+    }
+}
