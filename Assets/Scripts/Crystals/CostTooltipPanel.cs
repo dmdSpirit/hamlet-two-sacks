da160023b03ab09ab33726f6751bada053c7af75
Stas Volkov <doomedspirit.prog@gmail.com>
2023-09-12 16:44:50 +0700

    payment tooltip

---
+++ Assets/Scripts/Crystals/CostTooltipPanel.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using HamletTwoSacks.Infrastructure.StaticData;
+using HamletTwoSacks.Input;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+namespace HamletTwoSacks.Crystals
+{
+    public sealed class CostTooltipPanel : MonoBehaviour
+    {
+        // TODO (Stas): Localize this.
+        // - Stas 12 September 2023
+        [SerializeField]
+        private TMP_Text _text = null!;
+
+        [Inject]
+        private void Construct(StaticDataProvider staticDataProvider)
+        {
+            var inputConfig = staticDataProvider.GetConfig<InputConfig>();
+            InputAction payAction = inputConfig.PayAction;
+            _text.text = _text.text.Replace("[value]", payAction.GetBindingDisplayString());
+        }
+    }
+}
