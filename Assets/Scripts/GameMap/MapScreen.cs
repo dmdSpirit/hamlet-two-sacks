4952f5a5c60096d8c72d04030b4e1ba9ed127d1d
Stas Volkov <doomedspirit.prog@gmail.com>
2023-09-16 10:02:35 +0700

    map screen

---
+++ Assets/Scripts/GameMap/MapScreen.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+using dmdspirit.Core.UI;
+
+namespace HamletTwoSacks.GameMap
+{
+    public sealed class MapScreen : UIScreen
+    {
+        protected override void OnInitialize()
+        {
+            
+        }
+
+        protected override void OnShow()
+        {
+        }
+
+        protected override void OnHide()
+        {
+        }
+    }
+}
